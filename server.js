bd351366206683f35bd4c78e700dd0c4c167ee0c
yadud1001 <ann1001tampus@gmail.com>
2025-02-07 18:18:56 +0800

    added the edit to do route in the main server

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', todoController.todo_index);
 app.post('/', todoController.todo_addOrEditTask_post);
 app.get('/add-task', todoController.todo_addOrEditTask_get);
+app.get('/edit-task/:id', todoController.todo_addOrEditTask_get);
 app.get('/my-tasks', todoController.todo_myTasks);
 app.put('/my-tasks/:id/complete', todoController.todo_completeTask);
 app.delete('/my-tasks/clear-completed', todoController.todo_clearCompletedTasks);
