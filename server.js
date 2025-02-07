57cb8af87c39acd5a14a822c10f3a2817427dbd8
yadud1001 <ann1001tampus@gmail.com>
2025-02-07 16:47:29 +0800

    updated the main server to make the edit request work

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // todo view routes
 app.get('/', todoController.todo_index);
-app.post('/', todoController.todo_addTask_post);
-app.get('/add-task', todoController.todo_addTask_get);
+app.post('/', todoController.todo_addOrEditTask_post);
+app.get('/add-task', todoController.todo_addOrEditTask_get);
 app.get('/my-tasks', todoController.todo_myTasks);
 app.put('/my-tasks/:id/complete', todoController.todo_completeTask);
 app.delete('/my-tasks/clear-completed', todoController.todo_clearCompletedTasks);
